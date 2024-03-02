d7c84510a84a00e7c17a91aa87074f8187e95bea
truckdrivercz <eda.orvos@seznam.cz>
2024-03-02 09:35:41 +0100

    hodiny

--- res/js/main.js
+++ res/js/main.js
@@ -8,4 +8,4 @@ setInterval(()=>{
     hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
     min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
-}, 1000)
+}, 1000);
